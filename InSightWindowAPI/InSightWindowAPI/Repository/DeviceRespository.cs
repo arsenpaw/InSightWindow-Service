bb7be1f08ddfe36b5c35537c9ce821d64c6d7e46
Arsen Pavliuk <123877570+arsenpaw@users.noreply.github.com>
2024-12-02 20:19:52 +0200

    feat:  device repo base scturcure

---
+++ InSightWindowAPI/InSightWindowAPI/Repository/DeviceRespository.cs
@@ -0,0 +1,36 @@
+﻿using InSightWindowAPI.Models;
+using InSightWindowAPI.Models.DeviceModel;
+using InSightWindowAPI.Repository.Interfaces;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace InSightWindowAPI.Repository
+{
+    public class DeviceRespository : BaseRepository, IDeviceRepository
+    {
+        public DeviceRespository(UsersContext context): base(context)
+        {
+
+        }
+
+        public IQueryable<Device> GetAll()
+        => _context.Devices;
+
+        public IQueryable<Device> GetById(Guid deviceId) => _context.Devices.Where(x => x.Id == deviceId);
+            
+        public IQueryable<Device> GetByUserId(Guid userId) => _context.Devices.Where(x => x.UserId == userId);
+
+        public void Add(Device device) =>  _context.Devices.Add(device);
+
+        public void BindToUser(Device device, Guid userId)
+        {
+            device.UserId = userId;
+            _context.Devices.Update(device);
+        }
+
+        public void UnBindFromUser(Device device)
+        {
+            device.UserId = null;
+            _context.Devices.Update(device);
+        }
+    }
+}
